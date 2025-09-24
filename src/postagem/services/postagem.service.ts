cf8b889b6d98c5feb81e090c5abcb8ea5139f4c3
Gabriel Messias <gabrielmessias@outlook.com>
2025-09-24 14:24:49 -0300

    criando o modulo tema e relacionando com postagem

--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -2,16 +2,22 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
+import { TemaService } from './../../tema/services/tema.service';
 
 @Injectable()
 export class PostagemService {
   constructor(
     @InjectRepository(Postagem)
     private postagemRepository: Repository<Postagem>,
+    private temaService: TemaService,
   ) {}
 
   async findAll(): Promise<Postagem[]> {
-    return this.postagemRepository.find();
+    return await this.postagemRepository.find({
+      relations: {
+        tema: true,
+      },
+    });
   }
 
   async findById(id: number): Promise<Postagem> {
@@ -19,7 +25,11 @@ export class PostagemService {
       where: {
         id,
       },
+      relations: {
+        tema: true,
+      },
     });
+
     if (!postagem)
       throw new HttpException('Postagem não encontrada!', HttpStatus.NOT_FOUND);
 
@@ -31,20 +41,29 @@ export class PostagemService {
       where: {
         titulo: ILike(`%${titulo}%`),
       },
+      relations: {
+        tema: true,
+      },
     });
   }
 
   async create(postagem: Postagem): Promise<Postagem> {
+    await this.temaService.findById(postagem.tema.id);
+
     return await this.postagemRepository.save(postagem);
   }
 
   async update(postagem: Postagem): Promise<Postagem> {
     await this.findById(postagem.id);
+
+    await this.temaService.findById(postagem.tema.id);
+
     return await this.postagemRepository.save(postagem);
   }
 
   async delete(id: number): Promise<DeleteResult> {
     await this.findById(id);
+
     return await this.postagemRepository.delete(id);
   }
 }
